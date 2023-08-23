d0a0b47e55d18400f86c10a4433a47711bd5a7b4
iagoscm <iagomatosousa@gmail.com>
2023-08-23 19:10:11 -0300

    infos

--- tests/playwrightsigaa.spec.ts
+++ tests/playwrightsigaa.spec.ts
@@ -8,6 +8,13 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('Sigaa', () => {
+
+    // Não ocorre nenhum tipo de ação no site, apenas é feito um print da tela e comparado com o print anterior
+    // Caso alerte um erro, pode ser timeout (o qual pode ser configurado no config do playwright), ou pode ser porque há vaga
+    // No momento, só ocorre um erro, e não é dito qual é o erro, então não é possível saber se é timeout ou se há vaga
+    // Para saber se há vaga, é necessário olhar o erro, se for de print, significa que há vaga
+    // Para que seja útil, é interessante que seja mandado um alerta via email ou algo do tipo
+    // Para isso, é necessário que o teste seja feito em um servidor, e não localmente
     test('vendo se tem vaga em TEP; QS; GCES; COMP', async ({ page }) => {
         const sigaa = new Sigaa(page);
         while(true){
